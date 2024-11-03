69a14636b901f3f46399ed42ec70b82d84a1d42b
jennmcneill <jennifermcneill28@gmail.com>
2024-11-03 16:16:44 -0500

    import packages and set variables

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -6,6 +6,13 @@
 ##
 ## Usage: ImportArgos <ARGOS folder> <Output feature class> 
 ##
-## Created: Fall 2023
-## Author: John.Fay@duke.edu (for ENV859)
+## Created: Fall 2024
+## Author: jenn.mcneill@duke.edu
 ##---------------------------------------------------------------------
+
+# import packages
+import sys, os, arcpy
+
+# set input variables (hard-wired)
+inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
+outputFC = 'V:/ARGOSTracking/Scratch/ARGOSTrack.shp'
